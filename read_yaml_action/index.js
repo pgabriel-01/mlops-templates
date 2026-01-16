e05bd692dad1b908d3b127714eca810f9c773615
Perry Gabriel <pgabriel@berkeley.edu>
2026-01-16 15:35:38 -0500

    Add aml_compute_sku output to read_yaml_action and self-reference feature branch

--- read_yaml_action/index.js
+++ read_yaml_action/index.js
@@ -20,6 +20,7 @@ try {
     const postfix = String(configYaml["variables"]["postfix"]);
     const environment = String(configYaml["variables"]["environment"]);
     var enable_aml_computecluster = Boolean(configYaml["variables"]["enable_aml_computecluster"]);
+    var aml_compute_sku = String(configYaml["variables"]["aml_compute_sku"] || "STANDARD_D16S_V3");
     var enable_monitoring = Boolean(configYaml["variables"]["enable_monitoring"]);
     var resource_group = String(configYaml["variables"]["resource_group"]);
     var location = String(configYaml["variables"]["location"]);
@@ -80,6 +81,7 @@ try {
     core.setOutput("environment",environment);
     core.setOutput("enable_monitoring",enable_monitoring);
     core.setOutput("enable_aml_computecluster",enable_aml_computecluster);
+    core.setOutput("aml_compute_sku",aml_compute_sku);
     core.setOutput("resource_group",resource_group);
     core.setOutput("aml_workspace", aml_workspace);
     core.setOutput("bep", batch_endpoint_name);
